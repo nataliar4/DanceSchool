f733c25fdc13a85a87bf79431b4ba4a5536581bf
nataliar4 <natalia.rybialek@gmail.com>
2022-01-25 00:06:43 +0100

    Added assignments modification

--- listAssignments.js
+++ listAssignments.js
@@ -12,31 +12,34 @@ async function loadList() {
       }
     });
     const jsonAssignmentResponse = await assignmentResponse.json();
+    console.log(jsonAssignmentResponse);
     for (const response in jsonAssignmentResponse) {
       const newDiv = addDiv(assignmentsContainer);
       const instructorEmail = jsonAssignmentResponse[response].instructor;
       const courseName = jsonAssignmentResponse[response].course;
+      const earnings = jsonAssignmentResponse[response].earnings;
       addParagraph(newDiv, courseName);
       addParagraph(newDiv, "instructor: "+instructorEmail);
       addLabel(newDiv, "Instructor: ");
       addInput(newDiv, "instructorEmail", "email", instructorEmail, response);
-      addLabel(newDiv,"Course: ");
-      addInput(newDiv, "courseName", "text", courseName, response);
+      addLabel(newDiv, "Earnings: ");
+      addInput(newDiv, "earnings", "number", earnings, response, "0.01");
       
       addLinkCallback(newDiv, "Modify", "modifyElement", async () => {
         const instructorInput = document.querySelector(`#instructorEmail-input-${response}`);
         const courseInput = document.querySelector(`#courseName-input-${response}`);
 
           try {
-            const assignmentResponse = await fetch("http://localhost:3000/instructor/assignment/form/edit", {
-              method:"POST",
+            const assignmentResponse = await fetch("http://localhost:3000/instructor/assignment", {
+              method:"PUT",
               mode:"cors",
               credentials:"include",
               headers: {
                 'Content-Type': 'application/json'
                 // 'Content-Type': 'application/x-www-form-urlencoded',
               },
-              body:JSON.stringify({instructorEmail: instructorInput.value, courseName: courseInput.value})
+              body:JSON.stringify({instructorEmail: instructorEmail, courseName: courseName, 
+                earnings: earnings, instructorEmailNew: instructorInput.value, courseNameNew: courseName})
             });
             const jsonAssignmentResponse = await assignmentResponse.json();
             console.log(jsonAssignmentResponse);
